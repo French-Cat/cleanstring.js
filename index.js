183e370042edaf1153f7c587cc4e905a7d15c46c
FRCat.JS <89495053+French-Cat@users.noreply.github.com>
2022-04-26 14:36:20 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,2 @@
+if(typeof String.prototype.replaceAll=='undefined'){String.prototype.replaceAll=(match,replace)=>{return this.replace(new RegExp(match,'g'),()=>replace)}}
 function clean(a){const b=a.replaceAll("<","&lt"),c=b.replaceAll(">","&gt"),d=c.replaceAll("≤","&le"),e=d.replaceAll("≥","&ge");return e}module.exports=clean;
