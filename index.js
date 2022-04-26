fcb721c96c1fbdc985630530524ea8dc1660e39a
Aiden <amb@aspy.dev>
2022-04-26 14:25:56 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-function clean(a) {
+export default function clean(a) {
     const b = a.replaceAll("<", "&lt"),
         c = b.replaceAll(">", "&gt"),
         d = c.replaceAll("≤", "&le"),
         e = d.replaceAll("≥", "&ge");
     return e;
 }
-module.exports = clean;
