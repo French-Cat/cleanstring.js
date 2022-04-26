153304b8fac6f1a54c16f3f26201cbac00866aa9
FRCat.JS <89495053+French-Cat@users.noreply.github.com>
2022-04-26 14:24:10 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,1 @@
-function clean(a) {
-    const b = a.replaceAll("<", "&lt"),
-        c = b.replaceAll(">", "&gt"),
-        d = c.replaceAll("≤", "&le"),
-        e = d.replaceAll("≥", "&ge");
-    return e;
-}
-module.exports = clean;
+function clean(a){const b=a.replaceAll("<","&lt"),c=b.replaceAll(">","&gt"),d=c.replaceAll("≤","&le"),e=d.replaceAll("≥","&ge");return e}module.exports=clean;
