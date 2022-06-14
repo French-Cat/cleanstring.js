322676fb8001f9309e4df9b5e31b9ed6ddba6ac3
FRCat.JS <89495053+French-Cat@users.noreply.github.com>
2022-06-14 15:38:21 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,1 @@
-if(typeof String.prototype.replaceAll=='undefined'){String.prototype.replaceAll=(match,replace)=>{return this.replace(new RegExp(match,'g'),()=>replace)}}
-function clean(a){const b=a.replaceAll("<","&lt"),c=b.replaceAll(">","&gt"),d=c.replaceAll("≤","&le"),e=d.replaceAll("≥","&ge");return e}module.exports=clean;
+module.exports=(function(input){return input.replace(/</g, '&lt').replace(/>/g, '&gt').replace(/≤/g, '&le').replace(/≥/g, '&ge')})
